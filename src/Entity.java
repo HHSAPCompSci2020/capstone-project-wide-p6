87853d77bb521b32d5fcd0a9b79be761dc63a29e
Ethan Wang <ewang223@student.fuhsd.org>
2021-05-07 14:05:11 -0700

    slightly set up entity

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,10 @@
+import processing.core.PImage;
 
-public class Entity {
+public class Entity extends MovingImage{
+
+	public Entity(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
 
 }
